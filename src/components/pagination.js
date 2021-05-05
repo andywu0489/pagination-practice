fb5af1974199180e1c7219a3a51808fd542f1ecf
andywu0489 <andywu0489@gmail.com>
2021-05-04 23:57:56 -0400

    Use lodash chunk to split up array not while loop

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,136 +1,143 @@
-import React, {useState} from 'react'
-import './pagination.css'
+import React, { useState } from "react";
+import "./pagination.css";
+import _ from "lodash";
 
-export default function Pagination () {
-    const [currentPage, setCurrentPage] = useState(1)
-    const [number, setNumber] = useState(currentPage)
-    const [numPerPage, settNumPerPage] = useState(5)
-    const [orderByValue, setOrderByValue] = useState('accending')
-    const [filterValue, setFilterValue] = useState('none')
+export default function Pagination() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [number, setNumber] = useState(currentPage);
+  const [numPerPage, settNumPerPage] = useState(25);
+  const [orderByValue, setOrderByValue] = useState("accending");
+  const [filterValue, setFilterValue] = useState("none");
 
-    let arr = []
+  let arr = [];
 
-    for (let i = 1; i <= 100; i++) {
-        arr.push(i)
+  for (let i = 1; i <= 1000000; i++) {
+    arr.push(i);
+  }
+
+  if (orderByValue === "accending") {
+    arr.sort((a, b) => {
+      return a - b;
+    });
+  } else if (orderByValue === "decending") {
+    arr.sort((a, b) => {
+      return b - a;
+    });
+  }
+
+  if (filterValue === "even") {
+    arr = arr.filter((num) => {
+      return num % 2 === 0;
+    });
+  } else if (filterValue === "odd") {
+    arr = arr.filter((num) => {
+      return num % 2 !== 0;
+    });
+  }
+
+  const splicedArr = _.chunk(arr, numPerPage);
+
+  const currentPageArray = splicedArr[currentPage - 1];
+
+  const decrement = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      setNumber(currentPage - 1);
     }
+  };
 
-    if (orderByValue === 'accending') {
-        arr.sort((a, b) => {
-           return a - b
-        })
-    } else if (orderByValue === 'decending') {
-        arr.sort((a, b) => {
-            return b - a
-         })
+  const increment = () => {
+    if (currentPage < splicedArr.length) {
+      setCurrentPage(currentPage + 1);
+      setNumber(currentPage + 1);
     }
+  };
 
-    if (filterValue === 'even') {
-         arr = arr.filter(num => {
-           return num % 2 === 0
-        })
-    } else if (filterValue === 'odd') {
-        arr = arr.filter(num => {
-           return num % 2 !== 0
-        })
+  const handleOnKeyDown = (e) => {
+    if (e.key === "Enter" && number <= splicedArr.length && number > 0) {
+      setCurrentPage(number);
+      setNumber(number);
     }
+  };
 
-    const splicedArr = []
+  const handleChange = (e) => {
+    setNumber(parseInt(e.target.value, 10));
+  };
 
-    while (arr.length > 0) {
-        splicedArr.push(arr.splice(0, numPerPage))
-    }
+  const handleNumPerPageChange = (e) => {
+    settNumPerPage(parseInt(e.target.value, 10));
+    setCurrentPage(1);
+    setNumber(1);
+  };
 
-    const currentPageArray = splicedArr[currentPage - 1]
+  const handleOrderByChange = (e) => {
+    setOrderByValue(e.target.value);
+    setCurrentPage(1);
+    setNumber(1);
+  };
 
-    const decrement = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1)
-            setNumber(currentPage - 1)
-        }
-    }
+  const handleFilterValueChange = (e) => {
+    setFilterValue(e.target.value);
+    setCurrentPage(1);
+    setNumber(1);
+  };
 
-    const increment = () => {
-        if (currentPage < splicedArr.length) {
-            setCurrentPage(currentPage + 1)
-            setNumber(currentPage + 1)
-        }
-    }
-
-    const handleOnKeyDown = (e) => {
-        if (e.key === 'Enter' && number <= splicedArr.length && number > 0) {
-            setCurrentPage(number)
-            setNumber(number)
-        }
-    }
-
-    const handleChange = (e) => {
-        setNumber(parseInt(e.target.value, 10))
-    }
-
-    const handleNumPerPageChange = (e) => {
-        settNumPerPage(parseInt(e.target.value, 10))
-        setCurrentPage(1)
-        setNumber(1)
-    }
-
-    const handleOrderByChange = (e) => {
-        setOrderByValue(e.target.value)
-        setCurrentPage(1)
-        setNumber(1)
-    }
-
-    const handleFilterValueChange = (e) => {
-        setFilterValue(e.target.value)
-        setCurrentPage(1)
-        setNumber(1)
-    }
-
-    return(
-        <div className='pagination__container'>
-            <div className='pagination__settings'>
-            <span>{'Items Per Page: '}
-                <select onChange={handleNumPerPageChange} value={numPerPage}>
-                    <option value='5'>5</option>
-                    <option value='10'>10</option>
-                    <option value='20'>20</option>
-                </select>
-            </span>
-            <span>{'Order By: '}
-                <select onChange={handleOrderByChange} value={orderByValue}>
-                    <option value='accending'>Accending</option>
-                    <option value='decending'>Decending</option>
-                </select>
-            </span>
-            <span>{'Filter By: '}
-                <select onChange={handleFilterValueChange} value={filterValue}>
-                <option value='none'>---</option>
-                    <option value='even'>Even</option>
-                    <option value='odd'>Odd</option>
-                </select>
-            </span>
-            </div>
-            <div className='pagination__list'>
-                {currentPageArray && currentPageArray.map(number => {
-                    return (
-                        <p className='pagination__list-item'key={number}>{number}</p>
-                    )
-                })
-                }
-            </div>
-            <div className='pagination__controls'>
-                <button className='pagination__decrement-button' onClick={decrement}>-</button>
-                <span>
-                    <input className='pagination__page-input'
-                        type='number'
-                        name='currentPage'
-                        value={number}
-                        onKeyDown={handleOnKeyDown}
-                        onChange={handleChange}
-                    ></input>
-                    {`/${splicedArr.length}`}
-                </span>
-                <button className='pagination__increment-button'onClick={increment}>+</button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="pagination__container">
+      <div className="pagination__settings">
+        <span>
+          {"Items Per Page: "}
+          <select onChange={handleNumPerPageChange} value={numPerPage}>
+            <option value="25">25</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+        </span>
+        <span>
+          {"Order By: "}
+          <select onChange={handleOrderByChange} value={orderByValue}>
+            <option value="accending">Accending</option>
+            <option value="decending">Decending</option>
+          </select>
+        </span>
+        <span>
+          {"Filter By: "}
+          <select onChange={handleFilterValueChange} value={filterValue}>
+            <option value="none">---</option>
+            <option value="even">Even</option>
+            <option value="odd">Odd</option>
+          </select>
+        </span>
+      </div>
+      <div className="pagination__list">
+        {currentPageArray &&
+          currentPageArray.map((number) => {
+            return (
+              <p className="pagination__list-item" key={number}>
+                {number}
+              </p>
+            );
+          })}
+      </div>
+      <div className="pagination__controls">
+        <button className="pagination__decrement-button" onClick={decrement}>
+          -
+        </button>
+        <span>
+          <input
+            className="pagination__page-input"
+            type="number"
+            name="currentPage"
+            value={number}
+            onKeyDown={handleOnKeyDown}
+            onChange={handleChange}
+          ></input>
+          {`/${splicedArr.length}`}
+        </span>
+        <button className="pagination__increment-button" onClick={increment}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
